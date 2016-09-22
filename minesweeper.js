13f64d12b840506374474f91e1121c6ebe7428ee
gabrielamelian <gabrielamelian@gmail.com>
2016-09-22 15:31:57 +1200

    Add sound effects

--- minesweeper.js
+++ minesweeper.js
@@ -5,16 +5,14 @@ var board = {
     cells: []
 }
 
-
+//Load board and create on DOM
 function startGame () {
-    board.cells.length = 0
+    board.cells.length = 0;
     generateBoard();
-    for (var i = 0; i < board.cells.length; i++) {
-        board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
-    }
-    lib.initBoard()
-    document.addEventListener('click', checkForWin)
-    document.addEventListener('contextmenu', checkForWin)
+    addCountMines();
+    lib.initBoard();
+    document.addEventListener('click', checkForWin);
+    document.addEventListener('contextmenu', checkForWin);
 }
 
 // Populates the Board object with cells
@@ -33,21 +31,32 @@ function generateBoard () {
     }
 }
 
+//Adding a surrounding mines property to each cell
+function addCountMines () {
+    for (var i = 0; i < board.cells.length; i++) {
+        board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+    }
+}
+
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
-    var winner = false;
+    var winner = true;
+    var winSound = document.getElementById("win");
     for (var i = 0; i < board.cells.length; i++) {
-        if (board.cells[i].isMine && board.cells[i].isMarked) {
-            winner = true;
+        if (board.cells[i].isMine && !board.cells[i].isMarked) {
+            winner = false;
+            break;
         } else if (!board.cells[i].isMine && board.cells[i].hidden) {
             winner = false;
+            break;
         }
     }
     if (winner === true){
         lib.displayMessage('You win!');
+        winSound.play();
     }
 }
 
